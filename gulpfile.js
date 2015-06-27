04f0f7f37e86e8ca035cac8bd888b7b80ce3a27f
Sonny Lazuardi <sonnylazuardi@gmail.com>
2015-06-28 06:43:45 +0700

    add style gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var elixir = require('laravel-elixir');
+// var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,55 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.less('app.less');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var uglify = require("gulp-uglify");
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('vendor', function () {
+  gulp.src(['./lib/js/jquery.js','./lib/js/**/*.js'])
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'));
 });
+
+gulp.task('vendorCss', function () {
+  gulp.src('./sass/lib/**/*.css')
+    .pipe(concatCss('vendor.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./public/css'));
+});
+ 
+gulp.task('sass', function () {
+  gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('vendor:watch', function () {
+  gulp.watch('./lib/js/**/*.js', ['vendor']);
+}); 
+
+gulp.task('vendorCss:watch', function () {
+  gulp.watch('./sass/lib/**/*.js', ['vendorCss']);
+}); 
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'vendor', 'vendorCss', 
+  'sass:watch', 'vendor:watch', 'vendorCss:watch']);
+
+// elixir(function(mix) {
+//     mix.less('app.less');
+// });
+
+// elixir(function(mix) {
+//     mix.task('sass', './sass/*.scss');
+//     mix.task('vendor', './lib/js/**/*.js');
+//     mix.task('vendorCss', './sass/lib/**/*.js');
+// });
