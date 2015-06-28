858159905725a78ff949d9f8f459dca2b8f6ef66
Alif Raditya Rochman <alifradityar@gmail.com>
2015-06-28 19:38:50 +0700

    hello

--- resources/views/news/index2.blade.php
+++ resources/views/news/index2.blade.php
@@ -26,28 +26,28 @@
 	      		<img class="img-news-header2" src="{{asset('/img/spongebob.jpg')}}"></img>
           </div>
           <div class="col-md-9">
-	      		<div class="front-news-title2">
+	      		<div class="front-news-title2 even">
 	      			<h2> {{$item->title}}</h2>
 	      		</div>
-	      		<div class="front-news-date2">
+	      		<div class="front-news-date2 even">
 	      			<p> {{$item->timestamp}}</p>
 	      		</div>
-	      		<div class="front-news-description2">
-	      			<p>{!!str_limit($item->content, $limit = 250, $end = '')!!} <a href="news/single/{{$item->id}}">..See More..</a></p>
+	      		<div class="front-news-description2 even">
+	      			<p>{!!str_limit($item->content, $limit = 250, $end = '')!!} <a href="/news/single/{{$item->id}}">..See More..</a></p>
 	      		</div>
 	      	</div>
 	      </div>
         @else
         <div class="row">
           <div class="col-md-9">
-            <div class="front-news-title2">
+            <div class="front-news-title2 odd">
               <h2> {{$item->title}}</h2>
             </div>
-            <div class="front-news-date2">
+            <div class="front-news-date2 odd">
               <p> {{$item->timestamp}}</p>
             </div>
-            <div class="front-news-description2">
-              <p>{!!str_limit($item->content, $limit = 250, $end = '')!!} <a href="news/single/{{$item->id}}">..See More..</a></p>
+            <div class="front-news-description2 odd">
+              <p>{!!str_limit($item->content, $limit = 250, $end = '')!!} <a href="/news/single/{{$item->id}}">..See More..</a></p>
             </div>
           </div>
           <div class="col-md-3">
