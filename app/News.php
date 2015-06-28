c44ab395b7d073c80f3208b73eab987af69a73f8
Sonny Lazuardi <sonnylazuardi@gmail.com>
2015-06-28 19:43:04 +0700

    news

--- app/News.php
+++ app/News.php
@@ -7,12 +7,15 @@ use Cviebrock\EloquentSluggable\SluggableTrait;
 
 class News extends Model {
 
-<<<<<<< HEAD
+
     use SluggableTrait;
 
     protected $fillable = [
         'title',
         'content',
+        'longitude',
+        'latitude',
+        'pinned',
         'slug',
         'image',
     ];
@@ -27,29 +30,5 @@ class News extends Model {
     {
         return \Carbon\Carbon::createFromTimeStamp(strtotime($this->created_at))->diffForHumans();
     }
-=======
-  use SluggableTrait;
-
-  protected $fillable = [
-    'title',
-    'content',
-    'longitude',
-    'latitude',
-    'pinned',
-    'slug',
-    'image',
-  ];
-
-  protected $sluggable = [
-    'build_from' => 'title',
-    'save_to'    => 'slug',
-  ];
-	//
-
-  public function getTimestampAttribute()
-  {
-    return \Carbon\Carbon::createFromTimeStamp(strtotime($this->created_at))->diffForHumans();
-  }
->>>>>>> 858159905725a78ff949d9f8f459dca2b8f6ef66
 
 }
