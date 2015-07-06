1ec9fb3f8611a9a07c830ce1fdc14eba7bcefcd0
Alif Raditya Rochman <alifradityar@gmail.com>
2015-07-06 10:26:17 +0700

    hihi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,34 @@
 @include ('admin.header')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-            		@if (count($errors) > 0)
-									<div class="alert alert-danger">
-										<strong>Whoops!</strong> There were some problems with your input.<br><br>
-										<ul>
-											@foreach ($errors->all() as $error)
-												<li>{{ $error }}</li>
-											@endforeach
-										</ul>
-									</div>
-								@endif
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Silakan Login</h3>
+    <div id="login-page">
+        <div class="container">
+        
+              <form class="form-login" role="form" method="POST" action="{{ url('/auth/login') }}">
+                <h2 class="form-login-heading">sign in now</h2>
+                <div class="login-wrap">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus value="{{ old('email') }}">
+                    <br>
+                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                    <div class="checkbox">
+                        <label>
+                            <input name="remember" type="checkbox" value="Remember Me">Ingat Saya
+                        </label>
                     </div>
-                    <div class="panel-body">
-                        <form role="form" method="POST" action="{{ url('/auth/login') }}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus value="{{ old('email') }}">
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Ingat Saya
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <button type="submit" class="btn btn-lg btn-success btn-block">Masuk</button>
-
-                                <a class="btn btn-link" href="{{ url('/password/email') }}">Lupa password?</a>
-                            </fieldset>
-                        </form>
-                    </div>
+                    <label class="checkbox">
+                        <span class="pull-right">
+                            <a lass="btn btn-link" href="{{ url('/password/email') }}">Lupa password?</a>
+                        </span>
+                    </label>
+                    <button class="btn btn-theme btn-block" href="index.html" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
+                    <hr>
+                    
+        
                 </div>
-            </div>
+        
+              </form>       
+        
         </div>
-    </div>
+      </div>
 
 @include('admin.footer')
